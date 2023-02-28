80458f67c445dc2f4c1721a61c5be63a4e522ad9
Andrey <maven23@rambler.ru>
2023-02-28 23:00:08 +0300

    add personal and other

---
+++ app/Http/Requests/Admin/User/StoreRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests\Admin\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            /*'password' => 'required|string',*/
+            'role' => 'required|integer',
+        ];
+    }
+
+    public function messages() {
+        return [
+            'name.required' => 'Это поле необходимо для заполнения',
+            'name.string' => 'Имя должно быть строкой',
+            'email.required' => 'Это поле необходимо для заполнения',
+            'email.string' => 'Email должно быть строкой',
+            'email.email' => 'Поле Email должно соответствовать формату mail@some.domain',
+            'email.unique' => 'Пользователь с таким Email уже существует',
+           /* 'password.required' => 'Это поле необходимо для заполнения',
+            'password.string' => 'Пароль должнен быть строкой',*/
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,29 @@
 
 
     Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
-        Route::get('/', 'IndexController');
+        Route::get('/', 'IndexController')->name('main.index');
     });
 
-    Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function () {
+    Route::group(['namespace' => 'App\Http\Controllers\Personal', 'prefix' => 'personal', 'middleware' => ['auth', 'verified']], function () {
+        Route::group(['namespace' => 'Main' , 'prefix' => 'main'], function () {
+            Route::get('/', 'IndexController')->name('personal.main.index');
+        });
+        Route::group(['namespace' => 'Liked', 'prefix' => 'liked'], function () {
+            Route::get('/', 'IndexController')->name('personal.liked.index');
+            Route::delete('/{post}', 'DeleteController')->name('personal.liked.delete');
+        });
+        Route::group(['namespace' => 'Comment', 'prefix' => 'comment'], function () {
+            Route::get('/', 'IndexController')->name('personal.comment.index');
+            Route::get('/{comment}/edit', 'EditController')->name('personal.comment.edit');
+            Route::patch('/{comment}', 'UpdateController')->name('personal.comment.update');
+            Route::delete('/{comment}', 'DeleteController')->name('personal.comment.delete');
+        });
+    });
+
+
+    Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin', 'middleware' => ['auth', 'admin', 'verified']], function () {
         Route::group(['namespace' => 'Main'], function () {
-            Route::get('/', 'IndexController');
+            Route::get('/', 'IndexController')->name('admin.main.index');
         });
 
         Route::group(['namespace' => 'Post', 'prefix' => 'posts'], function () {
@@ -57,9 +74,20 @@
             Route::patch('/{tag}', 'UpdateController')->name('admin.tag.update');
             Route::delete('/{tag}', 'DeleteController')->name('admin.tag.delete');
         });
+
+        Route::group(['namespace' => 'User', 'prefix' => 'users'], function () {
+            Route::get('/', 'IndexController')->name('admin.user.index');
+            Route::get('/create', 'CreateController')->name('admin.user.create');
+            Route::post('/', 'StoreController')->name('admin.user.store');
+            Route::get('/{user}', 'ShowController')->name('admin.user.show');
+            Route::get('/{user}/edit', 'EditController')->name('admin.user.edit');
+            Route::patch('/{user}', 'UpdateController')->name('admin.user.update');
+            Route::delete('/{user}', 'DeleteController')->name('admin.user.delete');
+        });
+
     });
 
 
-    Auth::routes();
+    Auth::routes(['verify' => true]);
 
 
